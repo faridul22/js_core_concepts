324c18aee88733ae8793efc474373b571cc63db5
faridul22 <96929890+faridul22@users.noreply.github.com>
2023-11-12 10:13:30 +0600

    dot and bracket notation added

---
+++ notation.js
@@ -0,0 +1,20 @@
+const person = {
+    name: 'Lal Nil Holud mia',
+    profession: 'Traffic Surgeon',
+    age: 30,
+    son_name: 'bulu',
+    25: 'summer',
+    address: 'Shahabag'
+};
+
+// dot notation
+const prof1 = person.profession;
+
+// bracket notation
+const prof2 = person['profession'];
+const pName = 'profession';
+const prof3 = person[pName];
+const sonName = person.son_name;
+const session = person[25];
+console.log(session)
+console.log(sonName)
